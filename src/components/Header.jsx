ce328c9db188cd850bfd01708ed745081b50cea2
Mikhail Nepomnyashchiy <michey85@gmail.com>
2020-12-23 17:30:03 +0300

    header title fixed

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -5,7 +5,7 @@ function Header() {
         <nav className='green darken-1'>
             <div className='nav-wrapper'>
                 <Link to='/' className='brand-logo'>
-                    React Shop
+                    React Food
                 </Link>
                 <ul id='nav-mobile' className='right hide-on-med-and-down'>
                     <li>
